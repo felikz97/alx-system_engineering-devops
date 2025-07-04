46bfbb142a58228a5c33198a00dd4ebab39b111b
Felikz97 <felikz.kipkemoi@gmail.com>
2025-07-04 16:23:28 +0300

    puppet configuraion

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Using puppet to make changes to our configuration file
 
-file { 'etc/ssh/ssh_config': 
+file { 'ect/ssh/ssh_config': 
         ensure => present,
 
 content =>"
